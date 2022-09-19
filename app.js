a2dfd1356766b137984708f00ca177be15e80bfc
Taylor <taylor.sheets3@gmail.com>
2022-09-19 12:20:55 -0700

    mini-sprint complete

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// TODO
+const {useState, useEffect} = React;
+
+const GroceryListItem = (props) => {
+  const [isFound, setIsFound] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const style = {
+    textDecoration: isFound ? 'line-through' : 'none',
+    fontWeight: isHovered ? 'bold' : 'normal'
+  };
+
+  return (
+    <li style={style} onClick={() => setIsFound(!isFound)} onMouseEnter={() => setIsHovered(!isHovered)} onMouseOut={() => setIsHovered(!isHovered)}>
+      {props.groceries}
+    </li>
+  );
+};
+
+const GroceryList = (props) => (
+  <ul>
+    {props.groceries.map((grocery) => (
+      <GroceryListItem groceries = {grocery}/>
+    ))}
+  </ul>
+);
+
+const App = () => (
+  <div>
+    <h2>My grocery list</h2>
+    <GroceryList groceries = {['Strawberries', 'Bananas']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
